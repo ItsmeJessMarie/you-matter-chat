10398843381d196db2114d12b7ac15ec0365b4c8
ItsmeJessMarie <jboll262@gmail.com>
2024-08-08 19:37:04 -0500

    Turned links to donate off
    As website is not currently in active state, turned off donate links. Added a button to the bottom of each page.

---
+++ src/app.js
@@ -0,0 +1,18 @@
+let upbutton = document.getElementById("upBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    upbutton.style.display = "block";
+  } else {
+    upbutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
